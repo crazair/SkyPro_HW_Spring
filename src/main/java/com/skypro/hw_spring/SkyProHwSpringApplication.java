f1c24559d6054ac43451317ef68813115a045cc4
crazair <crazzyzyzy@rambler.ru>
2022-11-21 11:03:12 +0300

    first commit

---
+++ src/main/java/com/skypro/hw_spring/SkyProHwSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.hw_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProHwSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProHwSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/hw_spring/SkyProHwSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.hw_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProHwSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
